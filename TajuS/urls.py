1c1dc7fb5c986600766494814a6f74315692f18e
Hovo-93 <ovo.aroyan@mail.ru>
2023-03-24 18:18:48 +0300

    added settings.MEDIA_URL

--- TajuS/urls.py
+++ TajuS/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from jet import urls as jet_urls
+
+from django.conf import settings
+from usersdata.views import index
 
 urlpatterns = [
+
+    path('jet/', include(jet_urls, 'jet')),  # Django Jet urls
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
