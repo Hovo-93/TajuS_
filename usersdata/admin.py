32271bb8159eaeda93ab30bfc1db867d1d6b3266
Hovo-93 <ovo.aroyan@mail.ru>
2023-03-24 18:22:16 +0300

    added User Passport models

--- usersdata/admin.py
+++ usersdata/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from usersdata.models import User, Passports
+from django.utils.html import format_html
+
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'patronymic', 'birthday',
+                    'email', 'avatar', 'role','avatar_preview')
+
+
+class PassportModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'gender', 'serial', 'nationality', 'date_of_issue',
+                    'passport_expiration_date', 'img_preview')
+
+
+admin.site.register(User, UserModelAdmin)
+admin.site.register(Passports, PassportModelAdmin)
