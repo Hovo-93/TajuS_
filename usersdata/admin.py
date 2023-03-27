87fa1a3007ce8f699a4b10769c3392c334f996bb
Hovo-93 <ovo.aroyan@mail.ru>
2023-03-27 19:21:23 +0300

    added relation at admin

--- usersdata/admin.py
+++ usersdata/admin.py
@@ -2,13 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from usersdata.models import User, Passports
-from django.utils.html import format_html
 
-
-class UserModelAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'patronymic', 'birthday',
-                    'email', 'avatar', 'role','avatar_preview')
+from usersdata.models import User, Passport, MigrationCard
+from jet.admin import CompactInline
 
 
 class PassportModelAdmin(admin.ModelAdmin):
@@ -16,5 +12,32 @@ class PassportModelAdmin(admin.ModelAdmin):
                     'passport_expiration_date', 'img_preview')
 
 
+class MigrationCardModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'serial', 'number', 'citizenship', 'duration_of_stay_from',
+                    'duration_of_stay_to', 'card_preview')
+
+
+class PassportInline(CompactInline):
+    model = Passport
+    extra = 0
+
+
+class MigrationCardInline(CompactInline):
+    model = MigrationCard
+    extra = 0
+
+
+class UserModelAdmin(admin.ModelAdmin):
+    def has_passport(self, obj):
+        return obj.passport.exists()
+
+    has_passport.boolean = True
+    has_passport.short_description = 'Passport'
+    list_display = ('username', 'first_name', 'last_name', 'patronymic', 'birthday',
+                    'email', 'avatar_preview', 'role', 'has_passport')
+    inlines = [PassportInline, MigrationCardInline, ]
+
+
 admin.site.register(User, UserModelAdmin)
-admin.site.register(Passports, PassportModelAdmin)
+admin.site.register(Passport, PassportModelAdmin)
+admin.site.register(MigrationCard, MigrationCardModelAdmin)
