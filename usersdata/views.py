12759595e86b47041e1a1fb367668b8bdb8a6ef4
Hovo-93 <ovo.aroyan@mail.ru>
2023-03-27 19:22:28 +0300

    added test views

--- usersdata/views.py
+++ usersdata/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from usersdata.models import *
+
 
 # Create your views here.
+def users_with_passport(request):
+    users_with_passport = User.objects.filter(passport__isnull=False).distinct()
+    context = {
+        'users_with_passport': users_with_passport
+    }
+    return render(request, 'users_with_passport.html', context)
